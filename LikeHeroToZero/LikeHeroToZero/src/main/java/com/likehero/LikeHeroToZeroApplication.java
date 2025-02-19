9f3e496cf0396ae231644d799dcfa83e14bf5265
savocake <91053016+savocake@users.noreply.github.com>
2025-02-19 20:18:15 +0100

    1

---
+++ LikeHeroToZero/LikeHeroToZero/src/main/java/com/likehero/LikeHeroToZeroApplication.java
@@ -0,0 +1,13 @@
+package com.likehero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeHeroToZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeHeroToZeroApplication.class, args);
+	}
+
+}
---
+++ LikeHeroToZero/LikeHeroToZero/src/test/java/com/likehero/LikeHeroToZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likehero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeHeroToZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
